32c41325628ab951126c1e9dff7ac6d713ade3bc
Michael Alexander <alexandermw@gmail.com>
2014-11-21 14:39:17 -0800

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -3,8 +3,6 @@
 function getPuppies(){
   $.ajax( {
 
-    // The URL (path) for the request
-    // The below would submit to http://www.yoursite.com/post
     url: "http://rocky-dusk-3509.herokuapp.com/puppies.json",
 
     // the data to send (will be converted to a query string)
@@ -12,31 +10,24 @@ function getPuppies(){
     // data: {
     //     id: 123
     // },
-
-    // HTTP verb (aka "Type" of request)
     type: "GET",
-
-    // the type of data we expect back
     dataType : "json",
 
-    // Success callback to run if the request succeeds.
-    // The response is passed to the function
-    // as a variable, usually called `data` or `json`
     success: function( json ) {
       $('#puppy-list').empty();
-      json.forEach(function(puppy){
+      //newest to oldest
+      puppies = json.reverse();
+      
+      puppies.forEach(function(puppy){
+      
         //turn milliseconds into minutes
         minutesAgo = Math.floor(new Date(Date.now()- new Date(puppy.created_at)) / 60000);
         $puppyEntry = $("<li>"+puppy.name+"("+puppy.breed.name+"), created "+minutesAgo +" minutes ago -- <a href = ''>adopt</a></li>");
         $('#puppy-list').append($puppyEntry);
       });
-      console.log(stuff);
     },
 
     // Error callback to run if the request fails
-    // (e.g. server returns an error code like 301)
-    // The raw request and any status codes are 
-    // passed to the callback
     error: function( xhr, status, errorThrown ) {
         alert( "Sorry, there was a problem!" );
         console.log( "Error: " + errorThrown );
@@ -54,6 +45,4 @@ function getPuppies(){
 
 $(document).ready(function(){
   $('#refresh').click(getPuppies());
-
-
 });
